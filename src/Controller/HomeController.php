987883148327768ff491584b8083096c17ed8d9f
Jess Vachet <jess94i@hotmail.fr>
2018-11-04 19:05:28 +0100

    Second quest on synfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @route("/")
+     */
+    public function index()
+    {
+        return $this->render("Home.html.twig");
+    }
+}
